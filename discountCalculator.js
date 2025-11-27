e6dde5ad2102e1cfe2c9c134bd2a06c0385c4e43
Philip Olembo <pjole.kenya@gmail.com>
2025-11-27 09:45:45 +0300

    Lab: Debugging with the Debugger Statement - add debugger statements

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,10 +1,12 @@
+debugger;
 function calculateDiscountedPrice(quantity, pricePerItem) {
+    debugger;
     let totalPrice = 0;
     debugger;
     for (let i = 0; i < quantity; i++) {
         totalPrice += pricePerItem;
     }
-
+    debugger;
     if (quantity >= 10) {
         totalPrice *= 0.9;
     }
